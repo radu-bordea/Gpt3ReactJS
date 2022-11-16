2fe90d18d876273a78846bf5d32fd5135e96bd03
radu-bordea <77333219+radu-bordea@users.noreply.github.com>
2022-11-16 19:27:43 +0200

    First commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import './navbar.css'
+
+// BEM -> Block Element Modifier
+
+const Navbar = () => {
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
